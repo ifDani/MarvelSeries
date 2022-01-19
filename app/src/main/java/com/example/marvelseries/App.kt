2b264e6793660fb76816fce3ca8b4c3204f0b7fd
ifDani <dani.bargas97@gmail.com>
2022-01-19 20:22:32 +0100

    Configuring theme and padding insets

--- app/src/main/java/com/example/marvelseries/App.kt
+++ app/src/main/java/com/example/marvelseries/App.kt
@@ -6,4 +6,13 @@ import dagger.hilt.android.HiltAndroidApp
 @HiltAndroidApp
 class App: Application() {
 
+    //For access instance of the app from anywhere
+    companion object {
+        lateinit var instance: App private set
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        instance = this
+    }
 }
--- app/src/main/java/com/example/marvelseries/presentation/ui/MainActivity.kt
+++ app/src/main/java/com/example/marvelseries/presentation/ui/MainActivity.kt
@@ -1,24 +1,43 @@
 package com.example.marvelseries.presentation.ui
 
+
 import android.os.Bundle
+import android.provider.SyncStateContract
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.rememberNavController
+import com.example.marvelseries.R
 import com.example.marvelseries.presentation.ui.theme.MarvelSeriesTheme
+import com.google.accompanist.insets.ProvideWindowInsets
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             MarvelSeriesTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                ProvideWindowInsets {
+                    val systemUiController = rememberSystemUiController()
+                    SideEffect {
+                        systemUiController.setSystemBarsColor(Color.Transparent, darkIcons = false)
+                    }
+
+                    val navController = rememberNavController()
+                    val currentRoute =
+                        navController.currentBackStackEntryFlow.collectAsState(initial = navController.currentBackStackEntry)
+
+
                 }
+
             }
         }
     }
