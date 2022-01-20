334c9e6a6dbcc3395e5b27d58e3c398df49dccf9
ifDani <dani.bargas97@gmail.com>
2022-01-20 01:40:11 +0100

    Showing Data heroes in list

--- app/src/main/java/com/example/marvelseries/presentation/ui/characters/CharactersScreen.kt
+++ app/src/main/java/com/example/marvelseries/presentation/ui/characters/CharactersScreen.kt
@@ -1,6 +1,5 @@
 package com.example.marvelseries.presentation.ui.characters
 
-import android.util.Log
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
@@ -9,11 +8,12 @@ import androidx.compose.foundation.lazy.GridCells
 import androidx.compose.foundation.lazy.LazyVerticalGrid
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
@@ -21,6 +21,9 @@ import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import coil.compose.rememberImagePainter
 import com.example.marvelseries.domain.model.CharactersResponse
+import com.example.marvelseries.presentation.ui.theme.attrCharList
+import com.example.marvelseries.presentation.ui.theme.nameCharacter
+import com.example.marvelseries.presentation.ui.theme.titleStyle
 
 @ExperimentalFoundationApi
 @Composable
@@ -35,7 +38,7 @@ fun CharactersScreen(
             .background(Color.Black)
             .padding(horizontal = 15.dp)
     ) {
-        Text(text = "Menu", style = MaterialTheme.typography.body1, modifier = Modifier.padding(top = 48.dp, bottom = 10.dp))
+        Text(text = "Menu", style = titleStyle, modifier = Modifier.padding(top = 48.dp, bottom = 10.dp))
 
         LazyVerticalGrid(cells = GridCells.Fixed(2), contentPadding = PaddingValues(vertical = 8.dp, horizontal = 5.dp)) {
 
@@ -50,14 +53,32 @@ fun CharactersScreen(
 
 @Composable
 fun CharacterItem(character: CharactersResponse.Data.Result) {
-    Log.v("miapp","${character.thumbnail.path}.${character.thumbnail.extension}")
 
     Box(modifier = Modifier
         .padding(5.dp)
         .fillMaxWidth()
         .clip(RoundedCornerShape(10.dp))
         .aspectRatio(1f)) {
-//        Spacer(modifier = Modifier.background(Color.Red))
+
         Image(painter = rememberImagePainter(data = "${character.thumbnail.path}.${character.thumbnail.extension}"),contentScale = ContentScale.Crop, modifier = Modifier.fillMaxSize(), contentDescription = null )
+
+        Column(modifier = Modifier
+            .fillMaxSize()
+            .background(
+                brush = Brush.verticalGradient(
+                    colors = listOf(
+                        Color.Transparent,
+                        Color.Black.copy(alpha = 0.5f),
+                        Color.Black.copy(alpha = 0.8f),
+                    )
+                )
+            ).padding(horizontal = 10.dp).padding(bottom = 5.dp)) {
+            Row(horizontalArrangement = Arrangement.SpaceBetween, verticalAlignment = Alignment.Bottom, modifier = Modifier.weight(1f).fillMaxWidth()) {
+                Text(text = "Pelis: ${character.series.items.size}", style = attrCharList)
+                Text(text = "Comics: ${character.comics.items.size}", style = attrCharList)
+
+            }
+            Text(text = character.name, style = nameCharacter)
+        }
     }
 }
--- app/src/main/java/com/example/marvelseries/presentation/ui/theme/Type.kt
+++ app/src/main/java/com/example/marvelseries/presentation/ui/theme/Type.kt
@@ -5,16 +5,21 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 import com.example.marvelseries.R
 
 // Set of Material typography styles to start with
 val Typography = Typography(
     body1 = TextStyle(
-        fontFamily = FontFamily(Font(R.font.marvel_bold)),
-        fontSize = 34.sp,
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Normal,
+        fontSize = 16.sp,
         color = Color.White
-    )
+    ),
+
+
+
     /* Other default text styles to override
     button = TextStyle(
         fontFamily = FontFamily.Default,
@@ -27,4 +32,19 @@ val Typography = Typography(
         fontSize = 12.sp
     )
     */
+)
+val titleStyle = TextStyle(
+    fontFamily = FontFamily(Font(R.font.marvel_bold)),
+    fontSize = 34.sp,
+    color = Color.White
+)
+val nameCharacter = TextStyle(
+    fontFamily = FontFamily(Font(R.font.marvel_bold)),
+    fontSize = 24.sp,
+    color = Color.White
+)
+val attrCharList = TextStyle(
+    fontFamily = FontFamily(Font(R.font.marvel_bold)),
+    fontSize = 14.sp,
+    color = Color.White
 )
